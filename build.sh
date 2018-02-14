ba232039eac928644980e36da2fd9eab76efe82c
Elias Alves <unixelias@gmail.com>
2018-02-14 21:35:07 -0200

    Change branch tag

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ shopt -s extglob
 
 docker build -t unixelias/lamp:${TRAVIS_BRANCH} \
                    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-                   --build-arg BRANCHE_VCS=${TRAVIS_BRANCH} \
+                   --build-arg VCS_BRANCH=${TRAVIS_BRANCH} \
                    --build-arg VCS_REF=`git rev-parse --short HEAD` docker;                   
 
 docker push unixelias/lamp:${TRAVIS_BRANCH};
