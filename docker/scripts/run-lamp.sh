cf350731874b6c6bc0c5bbab8d8b057e1d015c0d
Elias Alves <unixelias@gmail.com>
2018-02-19 23:46:04 -0300

    Fix 2

--- docker/scripts/run-lamp.sh
+++ docker/scripts/run-lamp.sh
@@ -60,7 +60,7 @@ fi
 
 # Run Apache:
 if [ $LOG_LEVEL == 'debug' ]; then
-    /usr/sbin/apachectl -DFOREGROUND -k start -e debug
+    httpd-foreground #/usr/sbin/apachectl -k start -e debug
 else
-    &>/dev/null /usr/sbin/apachectl -DFOREGROUND -k start
+    httpd-foreground #&>/dev/null /usr/sbin/apachectl -DFOREGROUND -k start
 fi
